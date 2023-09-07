c29a5401e24f07e363dbe7f7adc9ddc0f295ad29
soufiane-bak <elbaktaouisoufiane@gmail.com>
2023-09-07 14:52:45 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry ponit
+ *
+ * Description: A C program that prints whith put function
+ *
+ * Return: Alawys 0 (success) 
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
